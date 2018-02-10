77c1b591e8fbf77ff53cdf0bb0b76237021794da
Drew Hess <src@drewhess.com>
2018-02-09 17:53:49 -0800

    Try a different approach to building my custom NixOps.
    This is based on IOHK's approach.
    
    https://github.com/input-output-hk/iohk-ops/blob/7aa3a88dbbb68ea1bb9770cb2d26a831c52a623e/default.nix#L16

--- overlays/nixops.nix
+++ overlays/nixops.nix
@@ -3,15 +3,28 @@ self: super:
 let
 
   inherit (super) buildEnv;
-  inherit (self) nixops;
+  localLib = import ../lib.nix;
 
 in
 {
+  nixops =
+    let
+      targetSystem = super.targetPlatform.system;
+      myNixOps = super.fetchFromGitHub {
+        owner =  "dhess";
+        repo =   "nixops";
+        rev =    "5014e17f5ab9a6eb481b705efb9bb8e0def3b738";
+        sha256 = "0rsn6xsr3hgy0szdim0if838bh1z7akbd4k8rik3bkmnwcrwlsgi";
+      };
+    in (import "${myNixOps}/release.nix" {
+      nixpkgs = localLib.fetchNixPkgs;
+    }).build.${targetSystem};
+
   nixops-env = buildEnv {
     name = "nixops-env";
     paths = [
-      nixops
+      self.nixops
     ];
-    meta.platforms = nixops.meta.platforms;
+    meta.platforms = self.lib.platforms.unix;
   };
 }
