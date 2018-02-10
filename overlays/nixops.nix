8708a643d8e6c8a1ef252c5ec1262a9d97b63edd
Drew Hess <src@drewhess.com>
2018-02-09 19:00:25 -0800

    Revert "Try a different approach to building my custom NixOps."
    This reverts commit 77c1b591e8fbf77ff53cdf0bb0b76237021794da.

--- overlays/nixops.nix
+++ overlays/nixops.nix
@@ -3,28 +3,15 @@ self: super:
 let
 
   inherit (super) buildEnv;
-  localLib = import ../lib.nix;
+  inherit (self) nixops;
 
 in
 {
-  nixops =
-    let
-      targetSystem = super.targetPlatform.system;
-      myNixOps = super.fetchFromGitHub {
-        owner =  "dhess";
-        repo =   "nixops";
-        rev =    "5014e17f5ab9a6eb481b705efb9bb8e0def3b738";
-        sha256 = "0rsn6xsr3hgy0szdim0if838bh1z7akbd4k8rik3bkmnwcrwlsgi";
-      };
-    in (import "${myNixOps}/release.nix" {
-      nixpkgs = localLib.fetchNixPkgs;
-    }).build.${targetSystem};
-
   nixops-env = buildEnv {
     name = "nixops-env";
     paths = [
-      self.nixops
+      nixops
     ];
-    meta.platforms = self.lib.platforms.unix;
+    meta.platforms = nixops.meta.platforms;
   };
 }
