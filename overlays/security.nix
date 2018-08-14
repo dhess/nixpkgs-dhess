aa3493af2a5cae4537c38220254d336dff480f59
Drew Hess <src@drewhess.com>
2018-08-13 22:58:13 -0700

    Yes, cfssl-1.3.2 is now upstream, but upstream is slightly broken.
    Specifically, it doesn't have x86_64-darwin in its platform. Override it.

--- overlays/security.nix
+++ overlays/security.nix
@@ -8,10 +8,14 @@ let
 in
 {
 
+  cfssl = super.cfssl.overrideAttrs (oldAttrs: {
+    meta.platforms = oldAttrs.meta.platforms ++ [ "x86_64-darwin" ];
+  });
+
   security-env = buildEnv {
     name = "security-env";
     paths = [
-      super.cfssl
+      self.cfssl
     ];
     meta.platforms = [ "x86_64-linux" "x86_64-darwin" ];
   };
